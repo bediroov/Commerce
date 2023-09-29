618b79cd9f6256875d8bd72873936002f8b02ab1
Elgun Ceferov <elgunceferov06@gmail.com>
2023-09-29 09:41:56 +0400

    dersde

---
+++ src/data/Carousel.js
@@ -0,0 +1,31 @@
+let carousel = [
+    {
+        id: 0,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/original/logo-default_1515811017__78747.original.png",
+        name:"logo_default"
+    },
+    {
+        id: 1,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/original/carousel/11/slide1__57732.jpg?c=2"
+    },
+    {
+        id: 2,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/original/carousel/9/slide2.jpg?c=2"
+    },
+    {
+        id: 3,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/original/carousel/12/slide3__55489.jpg?c=2"
+    },
+    {
+        id: 4,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/content/site/banner/home1/1.jpg"
+    },
+    {
+        id: 5,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/content/site/banner/home1/2.jpg"
+    },
+    {
+        id: 6,
+        url: "https://cdn11.bigcommerce.com/s-3zqjz60dg3/content/site/banner/home1/3.jpg"
+    },
+]
