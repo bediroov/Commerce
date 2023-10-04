a14015da79d2101c156b3041b7bbb9bf0495b736
Elgun Ceferov <elgunceferov06@gmail.com>
2023-10-04 19:05:49 +0400

    is about to end

--- src/data/Product.js
+++ src/data/Product.js
@@ -83,7 +83,7 @@ let data=[
         url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/126/459/13__31863.1523424292.jpg?c=2",
         name:"Kevinconsequat jalapeno pork tempus turpis",
         description:{
-            brand:"Common Good",
+            brand:"Common good",
             sku:"sport 6",
             condition:"new",
             weight:1,
@@ -135,7 +135,7 @@ let data=[
         url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/124/465/4__01585.1520036865.jpg?c=2",
         name:"Pastrami frankfurter nam tempus turpis",
         description:{
-            brand:"Common Good",
+            brand:"Common good",
             sku:"sport 6",
             condition:"new",
             weight:1,
@@ -324,6 +324,262 @@ let data=[
                 url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/100x100/products/128/455/j7__28590.1520036864.jpg?c=2"
             }
         ]
-    }
+    },
+    {
+        id:22,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/112/504/63__04614.1520059842.jpg?c=2",
+        name:"Syndra oriana noal",
+        description:{
+            brand:"OFS",
+            sku:"Copy of DPB",
+            condition:false,
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:4,
+        sale:false,
+        price:230,
+        kind:[
+            {
+                id:22.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/112/505/66__62178.1520059842.jpg?c=2"
+            },
+            {
+                id:22.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/112/506/65__13869.1520059842.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:23,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/119/481/7__13470.1520036875.jpg?c=2",
+        name:"Cow pastrami jowl",
+        description:{
+            brand:"Common good",
+            sku:"OCG 4",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:4,
+        sale:false,
+        price:300,
+        kind:[
+            {
+                id:23.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/119/482/9__17514.1520036875.jpg?c=2"
+            },
+            {
+                id:23.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/119/483/8__58897.1520036875.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:24,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/115/495/32__92961.1520036886.jpg?c=2",
+        name:"Denouncing pleasure",
+        description:{
+            brand:"Common good",
+            sku:"SM 15",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:4,
+        sale:true,
+        price:450,
+        kind:[
+            {
+                id:24.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/115/496/33__26381.1520059842.jpg?c=2"
+            },
+            {
+                id:24.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/115/497/36__24726.1520059842.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:25,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/118/487/14__45045.1520036886.jpg?c=2",
+        name:"Hazen Zimalan Raglan",
+        description:{
+            brand:"Common good",
+            sku:"OCG 3",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:3,
+        sale:true,
+        price:675,
+        kind:[
+            {
+                id:25.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/118/486/11__15764.1520036886.jpg?c=2"
+            },
+            {
+                id:25.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/118/485/13__44023.1520036886.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:26,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/113/503/57__19304.1520059842.jpg?c=2",
+        name:"Nautilus bresaola",
+        description:{
+            brand:"Common good",
+            sku:"Copy of SM13",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:2,
+        sale:true,
+        price:470,
+        kind:[
+            {
+                id:26.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/113/502/54__95309.1520059842.jpg?c=2"
+            },
+            {
+                id:26.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/113/501/55__73064.1520059842.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:27,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/114/498/37__46081.1520059842.jpg?c=2",
+        name:"Deserunt mollitia",
+        description:{
+            brand:"Common good",
+            sku:"SM14",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:3,
+        sale:false,
+        price:450,
+        kind:[
+            {
+                id:27.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/114/499/41__96981.1520059842.jpg?c=2"
+            },
+            {
+                id:27.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/114/499/41__96981.1520059842.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:28,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/116/492/19__95246.1524755441.jpg?c=2",
+        name:"Cuid densinteu",
+        description:{
+            brand:"Common good",
+            sku:"SM16",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:2,
+        sale:false,
+        price:450,
+        kind:[
+            {
+                id:28.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/116/493/20__37945.1520036886.jpg?c=2"
+            },
+            {
+                id:28.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/116/494/21__01934.1520036886.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:29,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/137/570/63__04614.1520036421.1280.1280__97449.1520072066.jpg?c=2",
+        name:"Donec et libero nunc quis facilisis odio",
+        description:{
+            brand:"Common good",
+            sku:"SM20",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:5,
+        sale:true,
+        price:654,
+        kind:[
+           
+        ]
+    },
+    {
+        id:30,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/181x181/products/120/478/1__56088.1520036875.jpg?c=2",
+        name:"Kenle masenitcute",
+        description:{
+            brand:"Common good",
+            sku:"OCG 5",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:5,
+        sale:true,
+        price:500,
+        kind:[
+            {
+                id:30.1,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/120/479/2__94135.1520072111.jpg?c=2"
+            },
+            {
+                id:30.2,
+                url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/41x41/products/120/480/3__09444.1520036875.jpg?c=2"
+            }
+        ]
+    },
+    {
+        id:31,
+        url:"https://cdn11.bigcommerce.com/s-3zqjz60dg3/images/stencil/500x659/products/136/567/57__19304.1520036381.1280.1280__20862.1520072005.jpg?c=2",
+        name:"Praesent at justo congue leo adipiscing",
+        description:{
+            brand:"Common good",
+            sku:"SM 18",
+            condition:"new",
+            weight:1,
+            minpurchase:1,
+            maxpurchase:10,
+            shipping:"Free Shipping"
+        },
+        rate:3,
+        sale:false,
+        price:454,
+        kind:[
+
+        ]
+    },
 
 ]
